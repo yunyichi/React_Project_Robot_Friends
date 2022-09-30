42e33bc03361fc9c943d05d419a7b2ba49b19e26
YUNYI CHI <yunyichi@YUNYIdeAir.lan>
2022-09-29 21:40:37 -0700

    initialize a repository

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App greeting = {'Hello' + ' React Noinja'}/>);
+reportWebVitals();
